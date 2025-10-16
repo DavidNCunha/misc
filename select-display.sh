568e558f8d193d0d3bb8d29aae5468576339c965
DavidNCunha <david@davidcunha.xyz>
2025-10-16 17:58:34 +0100

    Remove unused print statements

--- select-display.sh
+++ select-display.sh
@@ -3,7 +3,6 @@ outputs=($(xrandr | awk '/ connected/ {print $1}'))
 
 # Detect laptop screen (usually eDP or LVDS)
 laptop=$(xrandr | awk '/ connected/ && ($1 ~ /^eDP|^LVDS/) {print $1}')
-echo $laptop
 
 # Detect external screens (anything that's not the laptop)
 externals=()
@@ -11,11 +10,10 @@ for out in "${outputs[@]}"; do
     [[ "$out" != "$laptop" ]] && externals+=("$out")
 done
 
-
 # Case 1: No external monitors connected
 # Enable laptop monitor and exit.
 if [ ${#externals[@]} -eq 0 ]; then
-    echo "No external monitors detected. Enabling laptop screen ($laptop)..."
+    #echo "No external monitors detected. Enabling laptop screen ($laptop)..."
     xrandr --output "$laptop" --auto --primary
 #    for out in "${outputs[@]}"; do
 #        [ "$out" != "$laptop" ] && xrandr --output "$out" --off
@@ -25,7 +23,7 @@ fi
 
 #Case 2: If external monitors connected, select which monitor to display.
 keep=$(xrandr | awk '/ connected/ {print $1}' | dmenu -p "Select display to keep:")
-echo "You selected output: $keep" 
+#echo "You selected output: $keep" 
 
 # Exit if user canceled or made no selection
 [ -z "$keep" ] && exit
